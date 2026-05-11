b23e8e36d3e9514b647120184a3254218c0737f6
HassaanSiraj <hassaan.siraj.hs@gmail.com>
2026-05-11 16:51:45 -0400

    fix: health endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Health check
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "/up", to: proc { [200, {}, ["OK"]] }
 
   # API routes
   namespace :api do
